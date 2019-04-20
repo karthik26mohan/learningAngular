3d592d207a56bfd5f92caf03975f0f56039da220
Karthik Mohan <28807@nelnet.net>
2019-04-20 11:28:48 -0500

    Changes

---
+++ ng-fundamentals/src/app/app.module.ts
@@ -0,0 +1,68 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router'
+import { NgModule } from '@angular/core';
+
+import{
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDeatilsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver,
+  CreateSessionComponent,
+  SessionListComponent,
+  DurationPipe
+} from './events/index'
+import { EventsAppComponent } from './events-app.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { TOASTR_TOKEN, Toastr } from './common/toastr.service';
+import { appRoutes } from './routes';
+import { Error404Component } from './errors/404.component';
+import { AuthService } from './user/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CollapsibleWellComponent } from './common/collapsible-well.component';
+
+declare let toastr: Toastr
+
+@NgModule({
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventDeatilsComponent,
+    EventThumbnailComponent,
+    CreateEventComponent,
+    NavBarComponent,
+    Error404Component,
+    CreateSessionComponent,
+    SessionListComponent,
+    CollapsibleWellComponent,
+    DurationPipe
+  ],
+  providers: [
+    EventService,
+    { provide: TOASTR_TOKEN, useValue: toastr },
+    EventRouteActivator,
+    AuthService,
+    {
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkDirtyState
+    },
+    EventListResolver
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule { }
+
+export function checkDirtyState(component:CreateEventComponent){
+  if(component.isDirty){
+    return window.confirm('You have not saved this event, do you really want to cancel?')
+  }
+  return true
+}
---
+++ ng-fundamentals/src/app/events/events-list.resolve.service.ts
@@ -0,0 +1,15 @@
+import { EventService } from './shared/event.service';
+import { Resolve } from '@angular/router';
+import {map} from 'rxjs/operators'
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class EventListResolver implements Resolve<any>{
+    constructor(private eventService: EventService){
+
+    }
+
+    resolve(){
+        return this.eventService.getEvents().pipe(map(events=>events))
+    }
+}
